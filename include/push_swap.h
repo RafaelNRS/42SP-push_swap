daa375723aaa120a92cbb299e373428b5957ee10
RafaelNRS <rafaelr@drconsulta.com>
2023-02-22 20:50:10 -0300

    Add files via upload

---
+++ include/push_swap.h
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ranascim <ranascim@42.student.42sp.org.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/12 10:01:47 by ranascim          #+#    #+#             */
+/*   Updated: 2023/02/22 21:50:46 by ranascim         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+
+# include <libft.h>
+# include <push_swap.h>
+
+struct s_stack {
+	int value;
+	int index;
+	struct s_stack *next;
+	struct s_stack *prev;
+};
+
+typedef struct s_stack t_stack;
+
+void sa(t_stack **stack_a);
+void sb(t_stack **stack_b);
+void ss(t_stack **stack_a, t_stack **stack_b);
+void pa(t_stack **stack_a, t_stack **stack_b);
+void pb(t_stack **stack_a, t_stack **stack_b);
+void	ra(t_stack **stack_a);
+void	rb(t_stack **stack_b);
+void	rr(t_stack **stack_a, t_stack **stack_b);
+void	rra(t_stack **stack_a);
+void	rrb(t_stack **stack_b);
+void	rrr(t_stack **stack_a, t_stack **stack_b);
+
+t_stack *get_last(t_stack *stack);
+
+#endif
